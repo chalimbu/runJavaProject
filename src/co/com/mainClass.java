7993f0fd439d0bffbd56e5842db91e59f8447e1e
Sebastian Zapata <sebastian.zapata@geniussports.com>
2022-05-11 17:01:38 -0500

    using the comparator of java

--- src/co/com/mainClass.java
+++ src/co/com/mainClass.java
@@ -1,57 +1,48 @@
 package co.com;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
 
 public class mainClass {
-    public static void main(String args[]){
-       int[] input= {3,2,4,1,5,8,9,1,2,3};
-       quickSort(input);
-       printArray(input);
+
+    public static void main(String[] args){
+        int[] arrayToOrder={1,3,4,2,5,1,2,4,5};
+
+        bubleSort(arrayToOrder);
+
+        printArray(arrayToOrder);
     }
 
+    public static boolean bubleSort(int[] input){
+        boolean isInOrder=true;
+        if(input.length<=1){
+            return false;
+        }
+        do{
+            isInOrder=true;
+            for(int i=1;i<input.length;i++){
+                if(input[i-1]>input[i]){
+                    int temporal=input[i];
+                    input[i]=input[i-1];
+                    input[i-1]=temporal;
+                    isInOrder=false;
+                }
+            }
 
-    public static void quickSort(int[] input){
-        innerQuickSort(input,0,input.length);
+        }while(!isInOrder);
+
+        return true;
     }
 
     public static void printArray(int[] input){
-        for(int i=0;i<input.length;i++){
-            System.out.println("-"+input[i]);
-        }
-    }
-
-    private static void innerQuickSort(int[] input, int low, int max){
-        if(low<max){
-            int j=partition(input,low,max);
-
-            innerQuickSort(input,low,j);
-            innerQuickSort(input,j+1,max);
-        }
-    }
-
-    private static int partition(int[] input, int low, int max){
-        int i=low;
-        int j=max;
-        int pivot=input[low];
-
-        while(i<j){
-            do{
-                i++;
-            }while(i<input.length&&input[i]<=pivot);
-            do{
-                j--;
-            }while(j>=0&&input[j]>pivot);
-            if(i<j){
-                switchVariables(input,i,j);
+        if(input.length>0){
+            for(int i=0;i<input.length;i++){
+                System.out.print(" "+input[i]);
             }
         }
-        switchVariables(input,low,j);
-        return j;
     }
 
-    private static void switchVariables(int array[], int a, int b){
-        int temporal=array[a];
-        array[a]=array[b];
-        array[b]=temporal;
-    }
 }
 
 
